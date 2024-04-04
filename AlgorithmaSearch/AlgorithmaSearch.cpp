070da8041a0ed101a4be383b1b26e39e5c0b9f96
aivylv <lyvialarasati@gmail.com>
2024-04-04 12:24:19 +0700

    memeriksa apakah elemen yg dicari tidak ditemukan dalam array

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -51,6 +51,9 @@ void LinearSearch()
             }
             i++;    //step 4
         }
+        if (i == n)     //step 5
+            cout << "\n" << item << " not found in the array\n";
+        
     }
 }
 
